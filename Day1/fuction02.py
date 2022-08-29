3b893537c7ba5d6e6579644d7824588b7d932f16
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-29 16:02:58 +0900

    Day1

---
+++ Day1/fuction02.py
@@ -0,0 +1,20 @@
+# def add(a, b):
+#     result1 = a + b
+#     result2 = a * b
+#     return result1, result2  # 반환값이 두개일때
+#
+# res1, res2 = add(10, 20)
+# print(res1, res2)
+
+
+#함수만들기 문제
+#원의 넓이를 계산하는 함수를 정의하고, 원의 넓이를 return 받아 출력하는 코드를 만들어보세요
+#반지름이 10인 원과 반지름이 20 인 두 원의 면적의 합을 출력하세요
+def circle(r):
+    result = 3.14 * r ** 2
+    return result
+
+res1 = circle(10)
+res2 = circle(20)
+
+print('면적의 합 =', res1 + res2)
--- Day1/fuctionEx.py
+++ Day1/fuctionEx.py
@@ -1,5 +1,14 @@
 def add(a, b):
     result = a + b
-    print(result)
+    print('{} + {} = {}'.format(a,b, result))
 
-add(5,7)
+
+a = 10
+b = 20
+print('나는 {}살 입니다. 너는 {}살 입니다.'.format(a, b))
+
+print(f'나는 {a+b}살 입니다. 너는 {b}살 입니다.') #유용하다..
+
+print(f'{a} + {b} = {a+b}')
+
+add(a, b)
