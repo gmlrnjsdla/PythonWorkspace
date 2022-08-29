6d7844a02ab48427d22c3f167256596cfe7fd3b7
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-29 10:59:55 +0900

    Day1

---
+++ variable.py
@@ -0,0 +1,21 @@
+# age = 11   #숫자 변수 선언
+# name = '홍길동'   #문자 변수 선언
+# age2 = age + 1
+#
+# print('나는 10살 입니다.')
+# print('나는', 10, '살 입니다.')
+# print('나는', age, '살 입니다.')
+# print('내 이름은', name, '입니다. 나이는', age2, '입니다.')
+
+# mul = 10 * 10
+# power = 10 ** 3
+# reminder = 10 % 3
+#
+# print(mul, power, reminder)
+
+text1 = 'korea'
+text2 = 'fighting'
+text3 = '2000' + '3000'
+
+print(text1 + text2)
+print(text3)
