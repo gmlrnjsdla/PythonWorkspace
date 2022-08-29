9a8bcb8392dbfb0511a3e4e0881f3de984089d3b
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-29 12:48:21 +0900

    Day1

--- ifEx.py
+++ ifEx.py
@@ -1,5 +1,6 @@
-num = 100
+num = 1001
 #1번
+
 # if num % 2 == 0:
 #     print('num은 짝수입니다.')
 # else:
@@ -7,5 +8,32 @@ num = 100
 
 
 #2번
-if num % 4 == 0 and num % 10 == 0:
-    print('num은 4와 10의 공배수입니다!')
+
+# if num % 4 == 0 and num % 10 == 0:
+#     print('num은 4와 10의 공배수입니다!')
+#     print('안녕')
+#     print('반갑습니다.')
+
+
+# 블록 예제
+
+# if num % 2 == 0:
+#     print('num은 짝수입니다.')
+#     if num % 3 == 0:
+#         print('num은 4의 배수입니다.')
+#     if num % 5 == 0:
+#         print('num은 5의 배수입니다.')
+#     print('num은 100입니다.')
+#
+# print('이 줄은 if문과 상관없는 줄입니다.')
+
+
+
+#if-else예제
+
+# if num % 2 == 0:
+#     print('num은 짝수입니다.')  #조건식이 참일때 실행됨
+# else:
+#     print('num은 홀수입니다.')  #조건식이 거짓일 때 실행됨
+
+
