4b1fa679c7c2ffd56e4ec66bb4d19c14d96f0b9f
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-07 15:01:37 +0900

    Day2

---
+++ Day2/calculator.py
@@ -0,0 +1,17 @@
+# 사칙연산 함수 모듈
+
+def sum(a, b):
+    sum = a + b
+    return sum
+
+def mul(a, b):
+    mul = a* b
+    return mul
+
+def divide(a, b):
+    div = a / b
+    return div
+
+def minus(a, b):
+    minus = a - b
+    return minus
