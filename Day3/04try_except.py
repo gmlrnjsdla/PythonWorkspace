cf80bc09ec58da1287280e94b3fde1004a932817
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-08 14:42:11 +0900

    Day3

--- Day3/04try_except.py
+++ Day3/04try_except.py
@@ -27,32 +27,50 @@
 # print(list1)
 #===========================================================
 
-def divide(a, b):
-    try:
-        mok = a / b
-        print(mok)
-    except ZeroDivisionError:
-        print('0으로 나눌 수 없습니다.')
+# def divide(a, b):
+#     try:
+#         mok = a / b
+#         print(mok)
+#     except ZeroDivisionError:
+#         print('0으로 나눌 수 없습니다.')
 
 # divide(20, 0)
 
 #==========================================================
 
-def divide2(a, b):
-    if b==0:
-        print('0으로 나눌 수 없습니다.')
-    else:
-        mok = a / b
-        print(mok)
+# def divide2(a, b):
+#     if b==0:
+#         print('0으로 나눌 수 없습니다.')
+#     else:
+#         mok = a / b
+#         print(mok)
+#
+# num2 = input('100을 나눌 피젯수를 입력하세요 >> ')
+# num2 = int(num2)
+#
+# while num2 == 0:
+#     print('피젯수는 0이 될 수 없습니다!')
+#     num2 = input('100을 나눌 피젯수를 입력하세요 >> ')
+#     num2 = int(num2)
+#
+# divide2(20, num2)
 
-num2 = input('100을 나눌 피젯수를 입력하세요 >> ')
-num2 = int(num2)
+#=============================================================
 
-while num2 == 0:
-    print('피젯수는 0이 될 수 없습니다!')
-    num2 = input('100을 나눌 피젯수를 입력하세요 >> ')
-    num2 = int(num2)
+a = 10
+b = 20
+try:
+    mok = a / b
+    print(mok)
 
-divide(20,num2)
-divide2(20, num2)
+    text = 'abc'
+    print(int(text))
+# except Exception as ex:
+except Exception:
+    # print('오류 발생: ', ex)
+    import traceback
+    traceback.print_exc()
 
+print('프로그램 완료')
+
+#=========================================================
---
+++ Day3/05logic.py
@@ -0,0 +1,25 @@
+# kor = None
+# eng = None
+# math = None
+# sci = None
+#
+# kor = int(input('국어점수 입력 >>'))
+# eng= int(input('영어점수 입력 >>'))
+# math = int(input('수학점수 입력 >>'))
+# sci = int(input('과학점수 입력 >>'))
+#
+# # 4과목 모두 90점 이상일 때 장학금 대상
+# if kor >= 90 and eng >= 90 and math >= 90 and sci >= 90:
+#     print('장학금 대상 and')
+#
+# # 4과목 중 1과목만 90점 이상이면 장학금 대상
+# if kor >= 90 or eng >= 90 or math >= 90 or sci >= 90:
+#     print('장학금 대상 or')
+
+#============================================================
+
+a = 10
+b = None
+list = []
+print(bool(list))  # 0,None 을 제외한 모든 수는 TRUE
+
