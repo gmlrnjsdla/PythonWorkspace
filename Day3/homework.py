e3c087169acc2b156af536c7d543e6e7760622f8
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-14 10:28:12 +0900

    Day3

--- Day3/homework.py
+++ Day3/homework.py
@@ -17,15 +17,16 @@ life = 20
 count = 0
 print('남은 시도 횟수 : ', life)
 com = random.randint(1, 99)
+print(com)
 
 while True:
     user = int(input('컴퓨터가 생각한 수를 맞춰보세요(1~99) >> '))
-    if life == 1:
-        print('시도 횟수를 모두 사용하셨습니다. 정답은 {}였습니다. 게임을 종료합니다.'.format(com))
-        break
-    elif user == com:
+    if user == com:
         print("정답입니다. 게임을 종료합니다.")
         print('시도 횟수 : ', count)
+        break
+    elif life == 1:
+        print('시도 횟수를 모두 사용하셨습니다. 정답은 {}였습니다. 게임을 종료합니다.'.format(com))
         break
     elif user > com:
         print("틀렸습니다. 컴퓨터의 수는 {}보다 작은 수입니다.\n".format(user))
