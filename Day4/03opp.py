5b7bf96b7bbd57144819b50e3ef2e6b88ea047d1
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-14 15:35:46 +0900

    Day4

--- Day4/03opp.py
+++ Day4/03opp.py
@@ -21,30 +21,44 @@
 
 #===================================================================
 
-class Student:
-    name = '홍길동'    #클래스 변수, 멤버 변수, 속성, 필드
-    hakbun = '20220111'
-    gradeNum = 4
-    def pirntName(self):    # 메서드(methods)
-        print('학생 이름 : {}'.format(self.name))
-        print('학번 : {}'.format(self.hakbun))
-        print('학년 : {}'.format(self.gradeNum))
-
-student1 = Student()
-student1.name = '김유신'
-student2 = Student()
-
-student1.pirntName()
-student2.pirntName()
+# class Student:
+#     name = '홍길동'    #클래스 변수, 멤버 변수, 속성, 필드
+#     hakbun = '20220111'
+#     gradeNum = 4
+#     def pirntName(self):    # 메서드(methods)
+#         print('학생 이름 : {}'.format(self.name))
+#         print('학번 : {}'.format(self.hakbun))
+#         print('학년 : {}'.format(self.gradeNum))
+#
+# student1 = Student()
+# student1.name = '김유신'
+# student2 = Student()
+#
+# student1.pirntName()
+# student2.pirntName()
 
 #==================================================================
 
 class Circle:
-    pi = 3.14
 
-    def circleArea(self, radius):
-        area = radius * radius * self.pi
+    def __init__(self, radius, pi):  # 자동으로 실행/ 초기화자/ 초기값설정용 메서드
+        print('반지름 초기값 입력: {}'.format(radius))
+        self.radius = radius
+        self.pi = pi
+
+    def circleArea(self):
+        area = self.radius * self.radius * self.pi
         return area
 
-circle1 = Circle()
-print(circle1.circleArea(10))
+circle1 = Circle(10, 3.14)
+print(circle1.radius)
+print(circle1.circleArea())
+
+#==================================================================
+
+# 생성자(Constructor), 초기화자
+class Car:
+    def __init__(self):     # 초기화자(생성사)
+        print('초기화자 메서드가 호출됨')
+
+car = Car()     # Car 클래스로 car 인스턴스 생성
---
+++ Day4/04classTest.py
@@ -0,0 +1,56 @@
+# 삼각형 클래스 만들기
+# 초기값으로 밑변, 높이를 입력해서 인스턴스를 생성
+# 삼각형의 면적을 출력하는 메서드가 존재
+# 인스턴스를 2개 생성하고 각각 밑변 10, 높이 20
+# 밑변 20, 높이 40 인 삼각형의 면적을 출력하시오.
+
+class Triangle:
+    def __init__(self, width, height):
+        print('밑변 : {}'.format(width))
+        print('높이 : {}'.format(height))
+        self.width = width
+        self.height = height
+
+    def tri_area(self):
+        tri_area = self.width * self.height * 0.5
+        return tri_area
+
+area1 = Triangle(10, 20)
+print(area1.tri_area())
+area2 = Triangle(20, 40)
+print(area2.tri_area())
+
+#==========================================================================
+print('========================================')
+# 사칙연산 클래스 만들기
+# 두 개의 수를 초기값으로 입력하여 인스턴스 생성
+# 사칙연산을 하는 메서드 4개 생성
+# 20, 10 의 사칙연산 결과를 출력하도록 프로그램 작성
+
+class Calculator:
+    def __init__(self, a, b):
+        print('두개의 수 입력: {} {}'.format(a, b))
+        self.a = a
+        self.b = b
+
+    def sum(self):
+        sum = self.a + self.b
+        return sum
+
+    def minus(self):
+        minus = self.a - self.b
+        return minus
+
+    def mul(self):
+        mul = self.a * self.b
+        return mul
+
+    def divide(self):
+        div = self.a / self.b
+        return div
+
+cal1 = Calculator(20, 10)
+print(cal1.sum())
+print(cal1.minus())
+print(cal1.mul())
+print(cal1.divide())
