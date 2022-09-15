516474aedff6f793251189af663f74c1444233a1
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-15 16:40:46 +0900

    Day5

--- Day5/06test.py
+++ Day5/06test.py
@@ -32,7 +32,7 @@ print('===========================================')
 
 my_list = ["가", "나", "다", "라"]
 
-for i in range(3):
+for i in range(len(my_list)-1):
     print(my_list[i], my_list[i+1])
 
 
