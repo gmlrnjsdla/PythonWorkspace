2cf2f0afbd698c2b7bcbc4b9d6efc6c3a2b496e9
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-16 09:50:06 +0900

    Day5

--- Day5/07test.py
+++ Day5/07test.py
@@ -24,10 +24,10 @@ print('=================================================')
 
 
 def make_list(name):
-    name1 = [name[i] for i in range(len(name))]
+    name1 = [i for i in name]
     # name1 = []
-    # for i in range(len(name)):
-    #     name1.append(name[i])
+    # for i in name:
+    #     name1.append(i)
     return name1
 
 
@@ -43,11 +43,11 @@ su1 = [3, 4, 5, 6, 7, 8]
 
 
 def pickup_even(list):
-    list1 = [list[i] for i in range(len(list)) if list[i] % 2 == 0]
+    list1 = [i for i in list if i % 2 == 0]
     # list1 = []
-    # for i in range(len(list)):
-    #     if list[i] % 2 == 0:
-    #         list1.append(list[i])
+    # for i in list:
+    #     if i % 2 == 0:
+    #         list1.append(i)
     return list1
 
 
