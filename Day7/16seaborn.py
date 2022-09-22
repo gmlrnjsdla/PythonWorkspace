82e06bc29be2b5abc79fd5e88b6d9595755dde34
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-22 16:27:25 +0900

    Day7

--- Day7/16seaborn.py
+++ Day7/16seaborn.py
@@ -7,9 +7,23 @@ tips = sns.load_dataset('tips')
 # print(tips)
 # print(tips.describe())
 tips_day = tips.groupby('day').tip.sum()    # 요일별 팁의 합계
-# print(tips_day)
+print(tips_day)
+
+male_tip = tips[tips['sex']=='Male'].groupby('day').tip.sum()
+# 요일별 남자 팁의 합계
+female_tip = tips[tips['sex']=='Female'].groupby('day').tip.sum()
+# 요일별 여자 팁의 합계
+
 
 xlabel = ['Thur', 'Fri', 'Sat', 'Sun']
-sns.barplot(x = xlabel, y=tips_day)
+index = [0, 1, 2, 3]
+
+p1 = plt.bar(index, male_tip)
+p2 = plt.bar(index, female_tip)
+
+plt.legend((p1[0], p2[0]), ('Male', 'Female'))
+
+# sns.barplot(x=xlabel, y=tips_day)
+
 
 plt.show()
