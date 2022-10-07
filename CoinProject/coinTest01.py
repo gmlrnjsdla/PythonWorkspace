d86aa3d39ab4be7ad07ea11c331298b732718fed
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-10-07 19:25:28 +0900

    .

--- CoinProject/coinTest01.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.upbit.com/v1/market/all?isDetails=false"
-
-headers = {"accept": "application/json"}
-
-response = requests.get(url, headers=headers)
-
-print(response.text)
--- Day08/02googleTransTest.py
+++
@@ -1,7 +0,0 @@
-import googletrans  # 파이썬 구글번역 api 모듈
-
-trans = googletrans.Translator()
-
-trans_result = trans.translate('나는 한국사람입니다.', dest='en')
-
-print(trans_result.text)
